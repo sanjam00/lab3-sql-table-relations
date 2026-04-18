8a987fb58bb31e4cd2dce9450420b9fe9dd2420e
Sanaya James <sanaya.j.james@gmail.com>
2026-04-17 21:36:19 -0700

    begun steps 1 and 2, test not passing. completed steps 3 and 4, tests passing

--- main.py
+++ main.py
@@ -10,16 +10,47 @@ conn = sqlite3.connect('data.sqlite')
 pd.read_sql("""SELECT * FROM sqlite_master""", conn)
 
 # STEP 1
-df_boston = None
+df_boston = pd.read_sql("""
+                        SELECT firstName, lastName, jobTitle, officeCode, city
+                        FROM employees
+                        JOIN offices USING(officeCode)
+                        WHERE officeCode = "2";
+                        """, conn)
+# 
+# print(df_boston)
 
 # STEP 2
-df_zero_emp = None
+df_zero_emp = pd.read_sql("""
+                          SELECT COUNT(employeeNumber) AS numEmployees, city, officeCode
+                          FROM offices
+                          JOIN employees USING(officeCode)
+                          GROUP BY city
+                          HAVING numEmployees = 0;
+                          """, conn)
+
+# print(df_zero_emp)
 
 # STEP 3
-df_employee = None
+df_employee = pd.read_sql("""
+                          SELECT firstName, lastName, city, state
+                          FROM employees
+                          JOIN offices USING(officeCode)
+                          ORDER BY firstName, lastName
+                          """, conn)
+
+print(df_employee)
 
 # STEP 4
-df_contacts = None
+df_contacts = pd.read_sql("""
+                          SELECT c.contactFirstName, c.contactLastName, c.phone, c.salesRepEmployeeNumber
+                          FROM customers AS c
+                          LEFT JOIN orders AS o 
+                          ON c.customerNumber = o.customerNumber
+                          WHERE o.customerNumber IS NULL
+                          ORDER BY contactLastName;
+                          """, conn)
+
+print(df_contacts)
 
 # STEP 5
 df_payment = None
